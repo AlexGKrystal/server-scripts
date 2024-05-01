760ff6cd8d0160e94f854137961c7352e79ace8d
Alex Gough <alex.gough@krystal.uk>
2024-05-01 08:30:17 +0100

    directory changes and mysql service stops

--- mysql103/upgrade.sh
+++ mysql103/upgrade.sh
@@ -5,16 +5,17 @@
 
 # ------------ MySQL Backup START ------------ #
 echo "Running MySQL Backup..."
-whmapi1 configureservice service=mysql enabled=0 monitored=0
-mkdir -p /home/0_mysql_backup
-rsync -av --delete /var/lib/mysql /krystal-mysql-upgrade-backup/
 service mysql stop
-rsync -av --delete /var/lib/mysql /krystal-mysql-upgrade-backup/
+whmapi1 configureservice service=mysql enabled=0 monitored=0
+mkdir -p /home/krystal-mysql-upgrade-backup
+rsync -av --delete /var/lib/mysql /home/krystal-mysql-upgrade-backup/
+service mysql stop
+rsync -av --delete /var/lib/mysql /home/krystal-mysql-upgrade-backup/
 service mysql start
 echo "===================================================="
 echo "================ Backup folder view ================"
 echo "===================================================="
-ls -lah /home/0_mysql_backup/mysql
+ls -lah /home/krystal-mysql-upgrade-backup/mysql
 echo "MySQL MySQL Backup completed - Check above Output looks ok"
 whmapi1 configureservice service=mysql enabled=1 monitored=1
 # ------------ MySQL Backup END ------------ #
@@ -30,7 +31,7 @@ if [[ "$choice" =~ ^[yY]$ ]]; then
     /usr/share/lve/dbgovernor/mysqlgovernor.py --install
     # Backing up cnf and applying new config
     echo "replacing my.cnf (backup in /krystal-mysql-upgrade-backup/)"
-    cp /etc/my.cnf /krystal-mysql-upgrade-backup/
+    cp /etc/my.cnf /home/krystal-mysql-upgrade-backup/
     curl https://raw.githubusercontent.com/AlexGKrystal/server-scripts/main/mysql103/my.cnf > /etc/my.cnf
     service mysql restart
 
