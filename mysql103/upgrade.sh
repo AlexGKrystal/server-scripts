f29a8cc5a4da9bfcb3fe92ca7d11b58c9a8048ef
Alex Gough <alex.gough@krystal.uk>
2024-05-01 08:37:56 +0100

    removed folder creations, since a presync should already be running

--- mysql103/upgrade.sh
+++ mysql103/upgrade.sh
@@ -7,17 +7,14 @@
 echo "Running MySQL Backup..."
 service mysql stop
 whmapi1 configureservice service=mysql enabled=0 monitored=0
-mkdir -p /home/krystal-mysql-upgrade-backup
-rsync -av --delete /var/lib/mysql /home/krystal-mysql-upgrade-backup/
-service mysql stop
 rsync -av --delete /var/lib/mysql /home/krystal-mysql-upgrade-backup/
 service mysql start
+whmapi1 configureservice service=mysql enabled=1 monitored=1
 echo "===================================================="
 echo "================ Backup folder view ================"
 echo "===================================================="
 ls -lah /home/krystal-mysql-upgrade-backup/mysql
 echo "MySQL MySQL Backup completed - Check above Output looks ok"
-whmapi1 configureservice service=mysql enabled=1 monitored=1
 # ------------ MySQL Backup END ------------ #
 
 # ------------ MariaDB 10.6 Upgrade START ------------ #
