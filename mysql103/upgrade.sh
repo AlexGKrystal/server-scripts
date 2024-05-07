cd0c7957b363f7fcf8ba76b49af5a1c1133357b3
Alex Gough <alex.gough@krystal.uk>
2024-05-07 08:09:46 +0100

    put in a loop so user has to enter in Y or N to prevent skips being done accidentally

--- mysql103/upgrade.sh
+++ mysql103/upgrade.sh
@@ -24,24 +24,31 @@ echo "MySQL MySQL Backup completed - Check above Output looks ok"
 # ------------ MariaDB 10.6 Upgrade START ------------ #
 echo "##### !!!! IMPORTANT !!!! #####"
 echo "Please verify backups are ok and you are ready to proceed before continuing"
-read -p "Do you want to run MySQL upgrade? (y/N): " choice
-if [[ "$choice" =~ ^[yY]$ ]]; then
-    # Backing up existing my.cnf before upgrade
-    cp /etc/my.cnf /home/krystal-mysql-upgrade-backup/
-    #Upgrading MySQL
-    echo "Running MariaDB upgrade"
-    /usr/share/lve/dbgovernor/mysqlgovernor.py --mysql-version=mariadb106
-    /usr/share/lve/dbgovernor/mysqlgovernor.py --install
-    #Applying new Conf
-    echo "replacing my.cnf (backup in /krystal-mysql-upgrade-backup/)"
-    curl https://raw.githubusercontent.com/AlexGKrystal/server-scripts/main/mysql103/my.cnf > /etc/my.cnf
-    service mysql restart
+while true; do
+    read -p "Do you want to run MySQL upgrade? (y/N): " choice
+    case "$choice" in
+        [Yy]* )
+            # Backing up existing my.cnf before upgrade
+            cp /etc/my.cnf /home/krystal-mysql-upgrade-backup/
+            # Upgrading MySQL
+            echo "Running MariaDB upgrade"
+            /usr/share/lve/dbgovernor/mysqlgovernor.py --mysql-version=mariadb106
+            /usr/share/lve/dbgovernor/mysqlgovernor.py --install
+            # Applying new Conf
+            echo "Replacing my.cnf (backup in /krystal-mysql-upgrade-backup/)"
+            curl https://raw.githubusercontent.com/AlexGKrystal/server-scripts/main/mysql103/my.cnf > /etc/my.cnf
+            service mysql restart
 
-    echo "Upgrade Complete. MySQL Version:"
-    echo "##############################################################################"
-    mysql -V
-    echo "##############################################################################"
-else
-    echo "Skipping Upgrade"
-fi
+            echo "Upgrade Complete. MySQL Version:"
+            echo "##############################################################################"
+            mysql -V
+            echo "##############################################################################"
+            break;;
+        [Nn]* )
+            echo "Skipping Upgrade"
+            break;;
+        * )
+            echo "Please answer with y or n.";;
+    esac
+done
 # ------------ MariaDB 10.6 Upgrade END ------------ #
