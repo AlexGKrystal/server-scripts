dde123b03302b17cd36827f51b42c1fa4b0fc5df
Alex Gough <alex.gough@krystal.uk>
2024-07-30 08:51:29 +0100

    Test site loading times script

---
+++ speedtest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "URL to test:"
+read URL
+
+curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $URL > webspeedtest_"$(date)"
