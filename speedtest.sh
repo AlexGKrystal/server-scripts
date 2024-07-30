3869b2c75cfbd70f1768359ea1d87e66570d8557
Alex Gough <alex.gough@krystal.uk>
2024-07-30 08:54:08 +0100

    outputs Terminal instead of file

--- speedtest.sh
+++ speedtest.sh
@@ -3,4 +3,4 @@
 echo "URL to test:"
 read URL
 
-curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $URL > webspeedtest_"$(date)"
+curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $URL
