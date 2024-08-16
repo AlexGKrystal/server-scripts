edeb0b25b6e7c4ea7fbecf81fbea9b877e5dfa36
Alex Gough <alex.gough@krystal.uk>
2024-08-16 07:49:57 +0100

    created server-log_check.sh which gives me a broader output so I can see more results

---
+++ server-log_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Read log file.
+log_file=$1
+
+
+echo "#######################################################"
+echo "Please note, this log starts at:" `head -1 $log_file | awk '{print $4}' | tr -d [`
+echo "#######################################################"
+
+echo "~~~~~~~~~~~~~~~~~~~
+# Unique visitors #
+~~~~~~~~~~~~~~~~~~~"
+cat $log_file | awk '{print $1}' | sort | uniq -c | wc -l
+
+echo "~~~~~~~~~~~~~~~
+# top 40 ip's #
+~~~~~~~~~~~~~~~"
+cat $log_file | awk '{print $1}' |  sort -n | uniq -c | sort -n | tail -n 40
+
+
+echo "
+~~~~~~~~~~~~~~~~~~~~~~
+# Activity of top IP #
+~~~~~~~~~~~~~~~~~~~~~~"
+top_ip=`cat $log_file | awk '{print $1}' |  sort -n | uniq -c | sort -n | tail -n 1 | awk '{print $2}'`
+grep $top_ip $log_file | tail -n 5
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Top User agents #
+~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+cat $log_file | awk -F\" '($2 ~ "^GET /"){print $6}' | sort -n | uniq -c | sort -n | tail -n 20
+
+echo
+echo "
+~~~~ Data from log file: $log_file ~~~~"
