f1bc540df48a36a45905850be7064a7d964ade1e
Alex Gough <alex.gough@krystal.uk>
2024-08-16 09:17:18 +0100

    using cat and \before $ for the conf contents

--- monit.sh
+++ monit.sh
@@ -12,15 +12,16 @@ sed -i 's/set daemon  30/set daemon  10/g' /etc/monitrc
 touch /etc/monit.d/iowait.conf
 
 # Add in config
-tee /etc/monit.d/iowait.conf >/dev/null <<EOF
+cat << EOF > /etc/monit.d/iowait.conf
 set mailserver localhost # Use the local MTA
 
-set mail-format { from: monit@$HOST subject: Monit alert -- $EVENT $SERVICE message: Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION. }
+set mail-format { from: monit@\$HOST subject: Monit alert -- \$EVENT \$SERVICE message: Monit \$ACTION \$SERVICE at \$DATE on \$HOST: \$DESCRIPTION. }
 
 set alert tsfc@krystal.uk
 
-check system $HOST if cpu usage (wait) > 50% for 6 cycles then exec "/usr/bin/pkill -9 lsphp"
+check system \$HOST if cpu usage (wait) > 50% for 6 cycles then exec "/usr/bin/pkill -9 lsphp"
 EOF
+
 
 # Start and enable monit service
 systemctl start monit
