59a8ef88d869d636daa5ce3fef839d829e003043
Alex Gough <alex.gough@krystal.uk>
2024-08-16 09:52:22 +0100

    omment out lines which start the local web service

--- monit.sh
+++ monit.sh
@@ -8,6 +8,12 @@ yum install monit -y
 # Update /etc/monitrc to set daemon  10
 sed -i 's/set daemon  30/set daemon  10/g' /etc/monitrc
 
+# Comment out lines which start the local web service:
+sed -e '/set httpd port 2812/ s/^#*/#/' -i /etc/monitrc
+sed -e '/use address localhost/ s/^#*/#/' -i /etc/monitrc
+sed -e '/allow localhost/ s/^#*/#/' -i /etc/monitrc
+sed -e '/allow admin:monit/ s/^#*/#/' -i /etc/monitrc
+
 # Create file:
 touch /etc/monit.d/iowait.conf
 
