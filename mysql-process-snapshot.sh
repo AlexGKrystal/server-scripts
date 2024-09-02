1086fad48704e61eb9dd032b92ffd566dd4f5abb
Alex Gough <alex.gough@krystal.uk>
2024-09-02 11:18:56 +0100

    changed name and set a rm to clean up log

--- mysql-process-snapshot.sh
+++ mysql-process-snapshot.sh
@@ -7,7 +7,7 @@
 
 # Start logging processes in MySQL, pipe into a log and grab PID for job
 echo "Grabbing MySQL processes for 10 seconds"
-while true; do mysqladmin processlist | grep -v Sleep | grep _; done > mysql.log & 
+while true; do mysqladmin processlist | grep -v Sleep | grep _; done > mysql-process-snapshot.log & 
 TAIL_PID=$!
 # Wait for 10 seconds
 sleep 10
@@ -19,4 +19,5 @@ sleep 2
 
 # Grab Users collumn from log and output top 10 with number of hits
 echo "Checking mysql.log for top users..."
-cat mysql.log | awk '{print $4}' |  sort -n | uniq -c | sort -n | tail -n 10
+cat mysql-process-snapshot.log | awk '{print $4}' |  sort -n | uniq -c | sort -n | tail -n 10
+rm -f mysql-process-snapshot.log
