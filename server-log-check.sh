5ec24d5cf3d2f3b1a91806ca528dbb3f6c776de2
Alex Gough <alex.gough@krystal.uk>
2024-10-14 09:40:26 +0100

    updated log name and path. Log file doesn't delete so we can check further if needed

--- server-log-check.sh
+++ server-log-check.sh
@@ -4,30 +4,33 @@
 time=$1
 
 # get time for all logs into a single file
-grep "$time" /usr/local/apache/logs/domlogs/*-ssl_log > overview_log
+grep "$time" /usr/local/apache/logs/domlogs/*-ssl_log > ~/overview_log_temp.log
 
 echo "~~~~~~~~~~~~~~~~~~~
 # Unique visitors #
 ~~~~~~~~~~~~~~~~~~~"
-cat overview_log | awk '{print $1}' | sort | uniq -c | wc -l
+cat ~/overview_log_temp.log | awk '{print $1}' | sort | uniq -c | wc -l
 
 echo "~~~~~~~~~~~~~~~
 # top 40 ip's #
 ~~~~~~~~~~~~~~~"
-cat overview_log | awk '{print $1}' | awk -F':' '{print $2}' |  sort -n | uniq -c | sort -n | tail -n 40
+cat ~/overview_log_temp.log | awk '{print $1}' | awk -F':' '{print $2}' |  sort -n | uniq -c | sort -n | tail -n 40
 
 echo "
 ~~~~~~~~~~~~~~~~~~~~~~
 # Activity of top IP #
 ~~~~~~~~~~~~~~~~~~~~~~"
-top_ip=`cat overview_log | awk '{print $1}' |  sort -n | uniq -c | sort -n | tail -n 1 | awk '{print $2}'`
-grep $top_ip overview_log | tail -n 5
+top_ip=`cat ~/overview_log_temp.log | awk '{print $1}' |  sort -n | uniq -c | sort -n | tail -n 1 | awk '{print $2}'`
+grep $top_ip ~/overview_log_temp.log | tail -n 5
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Top User agents #
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cat overview_log | awk -F\" '($2 ~ "^GET /"){print $6}' | sort -n | uniq -c | sort -n | tail -n 20
+cat ~/overview_log_temp.log | awk -F\" '($2 ~ "^GET /"){print $6}' | sort -n | uniq -c | sort -n | tail -n 20
 
 # clean up file
-rm -f overview_log
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~
+raw log file in ~/overview_log_temp.log
+remove with: rm -f overview_log
+~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
