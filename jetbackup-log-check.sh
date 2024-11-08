0176971c9fcb07fb76b2519a6ed530a2b4993a6f
Alex Gough <alex.gough@krystal.io>
2024-11-08 09:17:40 +0000

    added spaces before pid and username so we can double double click to select

--- jetbackup-log-check.sh
+++ jetbackup-log-check.sh
@@ -18,7 +18,7 @@ if grep -q "JB Config" $recent_log; then
     for pid in $(grep ERROR $previous_log | awk '{print $5}' | tr -d '[],PID'); do
         username=$(grep $pid $previous_log | head -n 2 | tail -n 1 | awk 'NF>1{print $NF}')
         #output in a nice format
-        echo "PID:$pid - User:$username"
+        echo "PID: $pid - User: $username"
     done
 else
     # If "JB Config" is not found in the third line, search for "ERROR" in the most recent log file
@@ -32,7 +32,7 @@ else
     for pid in $(grep ERROR $recent_log | awk '{print $5}' | tr -d '[],PID'); do
         username=$(grep $pid $recent_log | head -n 2 | tail -n 1 | awk 'NF>1{print $NF}')
         #output in a nice format
-        echo "PID:$pid - User:$username"
+        echo "PID: $pid - User: $username"
     done
 fi
 
