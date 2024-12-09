e410e0214a4af2bf906b2c877620cc887be26589
Alex Gough <alex.gough@krystal.io>
2024-12-09 13:15:29 +0000

    new script for quickly identifying internal IPs and providing links/commands

---
+++ jet-network-script.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# 
+# Created by Alex G
+# DATE 09/12/2024
+
+# Get IP from Config
+IP_CHECK=`jetbackup5api -F listDestinations | grep host | awk -F': ' '{print $2}'`
+
+# Define the IPs to check against
+TARGET_IPS=("172.16.231.33" "172.16.231.47" "172.16.231.57")
+
+# Check if the IP matches any of the target IPs
+if [[ " ${TARGET_IPS[@]} " =~ " ${IP_CHECK} " ]]; then
+  echo "IP $IP_CHECK is a Internal."
+  echo "WHMLOGIN LINK:"
+  whmlogin
+  echo
+  echo "Run this on ark server ti whitelist this host"
+  echo "echo 'tcp|in|d=722|s=`ip addr show dev eth0 | grep inet | head -n 1 | awk '{print $2}' | cut -d'/' -f1` # Client Server - `hostname`' >> /etc/csf/csf.allow.include && csf -r"
+else
+  echo "ERROR! Internal IP not detected: $IP_CHECK"
+fi
+
+whmlogin;echo;echo "echo 'tcp|in|d=722|s=`ip addr show dev eth0 | grep inet | head -n 1 | awk '{print $2}' | cut -d'/' -f1` # Client Server - `hostname`' >> /etc/csf/csf.allow.include && csf -r"
+
+
+
+
+
+172.16.231.33
+172.16.231.47
+172.16.231.57
