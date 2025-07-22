b1f413051f1a79227373978836acfb2dd46143ac
Alex Gough <alex.gough@krystal.io>
2025-07-22 06:13:57 +0100

    added notice since range search won't be accurate to RIPE allocated range

--- log_check.sh
+++ log_check.sh
@@ -34,11 +34,14 @@ echo "~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~~"
 cat $log_file | awk -F\" '($2 ~ "^GET /"){print $6}' | sort -n | uniq -c | sort -n | tail
 
+echo "===================================================================================================
+Checking for Common Ranges. Below Ranges are not RIPE accurate, so please use WHOIS before blocking
+==================================================================================================="
+
 echo "~~~~~~~~~~~~~~~~~~
 # Top /24 Ranges #
 ~~~~~~~~~~~~~~~~~~"
 cat $log_file | awk '{split($1,a,"."); print a[1] "." a[2] "." a[3]}' | sort | uniq -c | sort -n | awk '$1>0{print $1 " hits from " $2".0/24"}' | tail
-
 
 echo "~~~~~~~~~~~~~~~~~~
 # Top /16 Ranges #
