468164c86914554ac9f5beb19ac06b11e6cfa969
Alex Gough <alex.gough@krystal.io>
2025-07-27 09:46:52 +0100

    cPanel username checks now search for a Message ID and show full log results for that

--- spam_check.sh
+++ spam_check.sh
@@ -4,12 +4,12 @@ read -rp "Enter a cPanel username or an email address: " user_input
 
 # Check if the input is an email address (i.e. contains @)
 if [[ "$user_input" == *"@"* ]]; then
-    echo " ===== Searching Exim logs ====="
+    echo "===== Searching Exim logs ====="
     grep "$user_input" /var/log/exim_mainlog | grep "A=dovecot_"
 
     # Get cPanel User
     echo ""
-    echo " ===== Getting cPanel Owner for Email ====="
+    echo "===== Getting cPanel Owner for Email ====="
     domain=$(echo "$user_input" | awk -F@ '{print $2}')
     owner=$(/scripts/whoowns "$domain")
     echo "cPanel account owner: $owner"
@@ -21,6 +21,10 @@ if [[ "$user_input" == *"@"* ]]; then
     echo "uapi --user=$owner Email passwd_pop email='$user_input' password='$new_pass'"
 else
     # Handle as cPanel username
-    echo " ===== Searching Exim logs ====="
+    echo "===== Searching Exim logs for User ====="
     grep "U=$user_input" /var/log/exim_mainlog
+
+    echo "===== Full Log for Latest Message ID ====="
+    message_id=$(grep "U=hoursunlock" /var/log/exim_mainlog | tail -n 1 | awk '{print $3}')
+    grep $message_id /var/log/exim_mainlog
 fi
