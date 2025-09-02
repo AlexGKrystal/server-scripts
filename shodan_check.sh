2c3dd1e914fa2e249fe2f65d6799a0f329188c34
Alex Gough <alex.gough@krystal.io>
2025-09-02 10:13:16 +0100

    Improved errors. entered todays date to example date and corrected WP backups script.

--- shodan_check.sh
+++ shodan_check.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 # Created by Alex G
-# DATE 15/07/2025
+# DATE 02/09/2025
 
 # Read log file.
 cp_user=$1
 
 if [ -z "$cp_user" ]; then
-  echo "Usage: $0 <cp_user>"
+  echo "Run script with user: /root/scripts/shodan_check.sh cp_user"
   exit 1
 fi
 
@@ -25,7 +25,7 @@ read -r option
 case "$option" in
   1)
     echo "==== Wordpress Files Found ===="
-    jq --arg user "$cp_user" '.[$user].cpanel_backups' /home/0_shodan_backup_manager/0_shodan/shodan_database.json
+    jq --arg user "$cp_user" '.[$user].wp_backups | to_entries | map(select(.value | length > 0)) | from_entries' /home/0_shodan_backup_manager/0_shodan/shodan_database.json
     echo "==== cPanel Backup files Found ===="
     jq --arg user "$cp_user" '.[$user].cpanel_backups' /home/0_shodan_backup_manager/0_shodan/shodan_database.json
     ;;
@@ -33,7 +33,7 @@ case "$option" in
     jq --arg user "$cp_user" '.[$user].cpanel_backups' /home/0_shodan_backup_manager/0_shodan/shodan_database.json
     ;;
   3)
-    echo "Enter the date to search (e.g. `tail -n 1 /var/log/shodan.log | awk '{print $1}'`):"
+    echo "Enter the date to search (e.g. Todays date: `tail -n 1 /var/log/shodan.log | awk '{print $1}'`):"
     read -r search_date
     awk -v user="$cp_user" -v date="$search_date" '$1 == date && $4 ~ user' /var/log/shodan.log
     ;;
