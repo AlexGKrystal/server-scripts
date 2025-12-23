7d71360633eec15c916c2bff0ac1e88a6c5b636e
Alex Gough <alex.gough@krystal.io>
2025-12-23 07:46:14 +0000

    updated script

--- cleanspace.sh
+++ cleanspace.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Usage
-# bash -c "$(wget -qO - https://deploy-managed.krystal.uk/scripts/cleanspace.sh)"
 
 echo -e "${bold}Usage Before Cleanup: ${normal}"
 df -h
@@ -15,25 +13,37 @@ sleep 0.5
 echo -e "\n${bold}Cleaning YUM Caches... ${normal}"
 rm -rf /var/cache/yum/*
 yum clean all
-echo -e "\n${bold}Cleaning Server...${normal} (clearing logs and other temp files) \n"
-(
+## Cleaning Files
 cd /root
-find /home/*/.trash/* -exec rm -rf {} \;
+echo "Emptying Trash..."
+find /home -maxdepth 2 -type d -name ".trash" -exec rm -rf {} +
+echo "Clearing Core Files..."
 find /home/* -name core.[0-9]* -exec rm -vf {} \;
+echo "Removing cPanel temp worker files..."
 rm -rf cpanel.TMP.work*
+echo "Clearing Clamav logs..."
 rm -rf clamav*
+echo "Clearing Spamd files..."
 rm -rf spamd*
+echo "Clearing cPanel files from users tmp dirs..."
 rm -rf /home/*/tmp/Cpanel_*
+echo "maldetect session files..."
 rm -rf /usr/local/maldetect/sess/*
+echo "Clearing cPanel restore files..."
 rm -rf /home/cprestore/*
+echo "Clearing MySQL Backup backup folder (often created for upgrades)..."
 rm -rf /mysqlbkp
+echo "Clearing error_log files..."
 find /home/ -type f -name "error_log" -exec rm {} \;
+echo "Clearing debug.log files..."
 find /home/ -type f -name "debug.log" -exec rm {} \;
+echo "Clearing Cpanel move files..."
 rm -rf /home/cpmove*
+echo "Clearing atop log files older than 2 days..."
 find /var/log/atop/ -mtime +2 -exec rm {} \;
+echo "Clearing unison.tmp files..."
 find /home/ -name "*.unison.tmp" > /root/unisontmp.log
 for u in `cat unisontmp.log`;do rm -rf $u;done
-) >/dev/null 2>&1 2>/dev/null & pid=$!
 
 echo "Searching for cPanel Backups over 31 days old..."
 find /home/* -maxdepth 1 -name "backup-*.tar.gz" -type f -ctime +31 -exec rm {} \;
